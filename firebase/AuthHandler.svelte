0e6389d788db542c8995ae1c6a94174141ec8de4
Clay DeGruchy <clay.degruchy+github@gmail.com>
2024-08-27 12:08:46 +0200

    init

---
+++ firebase/AuthHandler.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { FirebaseApp, collectionStore, userStore } from "sveltefire";
+  import { SignedIn, SignedOut, Doc, Collection } from "sveltefire";
+  import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+  import type { Auth } from "firebase/auth";
+
+  import { firestore, auth } from "./firebase";
+  import StatusIndicator from "./StatusIndicator.svelte";
+
+  async function signInWithGoogle(auth: Auth) {
+    const provider = new GoogleAuthProvider();
+    provider.addScope("profile");
+    provider.addScope("email");
+    await signInWithPopup(auth, provider);
+  }
+</script>
+
+<FirebaseApp {auth} {firestore}>
+  <SignedIn let:user let:signOut>
+    <StatusIndicator />
+
+    <button on:click={signOut}>👤 {user.displayName}</button>
+  </SignedIn>
+
+  <SignedOut let:auth>
+    <button on:click={() => signInWithGoogle(auth)}>Sign In</button>
+    <h3>Sign in to continue</h3>
+  </SignedOut>
+
+  <slot />
+</FirebaseApp>
---
+++ firebase/StatusIndicator.svelte
@@ -0,0 +1,49 @@
+<script>
+  import { firebaseConnectionState } from "./firebase";
+  import { SignedIn } from "sveltefire";
+</script>
+
+<SignedIn>
+  <small>
+    <div class="float">
+      <div class={$firebaseConnectionState ? " shimmer-bar" : ""}></div>
+    </div>
+    <div>
+      {$firebaseConnectionState || "Saved"}
+    </div>
+  </small>
+</SignedIn>
+
+<style>
+  .float {
+    position: fixed;
+    top: 0;
+    width: 100%;
+  }
+
+  .shimmer-bar {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 4px;
+    background: linear-gradient(
+      90deg,
+      rgba(255, 223, 0, 0) 25%,
+      rgba(255, 223, 0, 0.5) 50%,
+      rgba(255, 223, 0, 0) 75%
+    );
+    background-size: 200% 100%;
+    animation: shimmer 1.5s infinite;
+    z-index: 9999;
+  }
+
+  @keyframes shimmer {
+    0% {
+      background-position: -200% 0;
+    }
+    100% {
+      background-position: 200% 0;
+    }
+  }
+</style>
